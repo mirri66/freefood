ee8fa7dc187ecd0d09bc89a2363e2d69d641d253
Grace Tang <grace@dn0a208c41.sunet>
2012-10-27 16:25:05 -0700

    update food feed

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Freefood::Application.routes.draw do
 
   resources :users
 
+  root to: 'foods#index'
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
