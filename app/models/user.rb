a7f577208fb5f77be69434d66ba7bbe0207db291
Grace Tang <grace@dn521372.sunet>
2012-10-28 12:40:41 -0700

    email validation, map

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ActiveRecord::Base
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
+
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@stanford\.edu+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
  
   private
 
